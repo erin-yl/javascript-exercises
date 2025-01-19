e7bd2f913bd7a24a2d0de78fb7c1898542556f51
erin-yl <erinyinling@gmail.com>
2025-01-19 14:08:42 -0800

    Add exercise file

---
+++ calendar.js
@@ -0,0 +1,46 @@
+const select = document.querySelector("select");
+const list = document.querySelector("ul");
+const h1 = document.querySelector("h1");
+
+select.addEventListener("change", () => {
+  const choice = select.value;
+  createCalendar(choice);
+});
+
+function createCalendar(month) {
+  let days = 31;
+
+  switch (month) {
+    case "February":
+      days = 28;
+      break;
+    case "April":
+    case "June":
+    case "September":
+    case "November":
+      days = 30;
+      break;
+  }
+
+  if (month === "February") {
+    days = 28;
+  } else if (
+    month === "April" ||
+    month === "June" ||
+    month === "September" ||
+    month === "November"
+  ) {
+    days = 30;
+  }
+
+  list.textContent = "";
+  h1.textContent = month;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+select.value = "January";
+createCalendar("January");
