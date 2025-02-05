b2124bd5fd5f318a34e168e4a20425ac1e9fe6cc
erin-yl <erinyinling@gmail.com>
2025-02-05 15:57:30 -0800

    Refine comments

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,32 +1,26 @@
 // Pseudocode:
-// User inputs their choice of rock, paper, or scissors
+// Human inputs their choice of rock, paper, or scissors
 // Otherwise show "Please enter rock, paper, or scissors."
 // Computer returns a choice of rock, paper, or scissors
-// If user inputs rock and computer returns scissors, print "You win! Rock beats scissors."
-// If user inputs rock and computer returns paper, print "You lose! Paper beats rock."
-// If user inputs scissors and computer returns paper, print "You win! Scissors beats paper."
-// If user inputs scissors and computer returns rock, print "You lose! Rock beats scissors."
-// If user inputs paper and computer returns rock, print "You win! Paper beats rock."
-// If user inputs paper and computer returns scissors, print "You lose! Scissors beats paper."
+// If human wins, print "You win! [human choice] beats [computer choice]."
+// If computer wins, print "You lose! [computer choice] beats [human choice]."
 // Otherwise show "It's even."
 // Play 5 rounds, calculate scores, and declare a final winner
 
 let humanScore = 0;
 let computerScore = 0;
 
-// Function to get user's choice
 function getHumanChoice() {
-  let userInput = prompt("Rock, paper, or scissors?").toLowerCase();
+  let humanInput = prompt("Rock, paper, or scissors?").toLowerCase();
 
-  if (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors") {
-    userInput = prompt("Please enter rock, paper, or scissors.").toLowerCase();
-    return userInput;
+  if (humanInput !== "rock" && humanInput !== "paper" && humanInput !== "scissors") {
+    humanInput = prompt("Please enter rock, paper, or scissors.").toLowerCase();
+    return humanInput;
   } else {
-    return userInput;
+    return humanInput;
   } 
 }
 
-// Function to get computer's choice
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
 
@@ -39,7 +33,6 @@ function getComputerChoice() {
   }
 }
 
-// Function to play a single round
 function playRound(humanChoice, computerChoice) {
   if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
@@ -61,7 +54,6 @@ function playRound(humanChoice, computerChoice) {
   console.log("Your score: " + humanScore + ". Computer's score: " + computerScore + ".");
 }
 
-// Function to play 5 rounds
 function playGame() {
   for (let round = 0; round < 5; round++) {
     const humanSelection = getHumanChoice();
