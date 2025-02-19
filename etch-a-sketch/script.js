683624672167bf3e8dc2ee06bccd04d75b570f3c
erin-yl <erinyinling@gmail.com>
2025-02-19 15:13:54 -0800

    Create hover effect

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,8 +1,22 @@
 const container = document.querySelector("#container");
+const button = document.querySelector(".button");
 
 for (let i = 0; i < 16 * 16; i++) {
   const squareDiv = document.createElement("div");
-  squareDiv.style.border = "1px solid black";
-  squareDiv.style.aspectRatio = "1 / 1";
+  squareDiv.classList.add("square");
   container.appendChild(squareDiv);
+
+  squareDiv.addEventListener("mouseover", () => {
+    squareDiv.style.backgroundColor = "#f3d8c7";
+  });
 }
+
+button.addEventListener("click", () => {
+  let num = prompt("Please enter a number less than 100: ", "");
+
+  if ( num < 0 || num > 100 || !Number.isInteger(num)) {
+    num = prompt("Please enter a valid number: ", "")
+  } else {
+    document.body.removeChild(container);
+  }
+});
