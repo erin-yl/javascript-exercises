f1704583e68134be600ebd32135bf1968c97f9e1
erin-yl <erinyinling@gmail.com>
2025-02-20 16:25:08 -0800

    Complete exercise three

--- objects/objects-basics.js
+++ objects/objects-basics.js
@@ -1,14 +1,31 @@
+// Exercise one: Hello, object
 let user = {
-  "name": "John",
-  "surname": "Smith",
+  name: "John",
+  surname: "Smith",
 }
 
 user.name = "Pete";
 delete user.name;
 
+// Exercise two: Check for emptiness
 function isEmpty(obj) {
   for (let key in obj) {
     return false;
   }
   return true;
 }
+
+// Exercise three: Sum object properties
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+  return sum;
+}
+
+// Exercise four: Multiply numeric property values by 2
