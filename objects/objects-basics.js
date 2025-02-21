dde8faad20965da35328b9e097674a5dd5e9fb88
erin-yl <erinyinling@gmail.com>
2025-02-21 15:29:32 -0800

    Complete exercise five

--- objects/objects-basics.js
+++ objects/objects-basics.js
@@ -36,3 +36,19 @@ function multiplyNumeric(obj) {
     }
   }
 }
+
+// Exercise five
+const cat = {
+  name : 'Bertie',
+  breed : 'Cymric',
+  color : 'white',
+  greeting: function() {
+    console.log('Meow!');
+  }
+}
+
+const catName = cat.name;
+cat.greeting();
+cat.color = 'black';
+
+// Exercise six
