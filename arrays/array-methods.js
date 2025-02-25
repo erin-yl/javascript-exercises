fc1adb2a3d96ea76ea3633a81fd4838cda0fd91e
erin-yl <erinyinling@gmail.com>
2025-02-25 10:55:48 -0800

    Complete exercise two

--- arrays/array-methods.js
+++ arrays/array-methods.js
@@ -5,3 +5,8 @@ function camelize(str) {
     .map((word, index) => index == 0? word : word[0].toUpperCase() + word.slice(1))
     .join('');
 }
+
+// Exercise 2
+function filterRange(arr, a, b) {
+  return arr.filter(el => (el >= a && el <= b));
+}
