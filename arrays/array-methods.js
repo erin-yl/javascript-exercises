1c11677cdf4e224b0970e37147d66553628c371e
erin-yl <erinyinling@gmail.com>
2025-02-25 18:28:25 -0800

    Complete exercise three

--- arrays/array-methods.js
+++ arrays/array-methods.js
@@ -10,3 +10,16 @@ function camelize(str) {
 function filterRange(arr, a, b) {
   return arr.filter(el => (el >= a && el <= b));
 }
+
+// Exercise 3
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i];
+
+    if (el < a || el > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
