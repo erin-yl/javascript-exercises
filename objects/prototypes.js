aa32c04cfb8c606d0dbb9d7a4c3922d8f9fe239d
erin-yl <erinyinling@gmail.com>
2025-04-17 15:27:58 -0700

    Complete exercise two

--- objects/prototypes.js
+++ objects/prototypes.js
@@ -1,1 +1,34 @@
 // Exercise 1
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3
+};
+Object.setPrototypeOf(table, head);
+
+let bed = {
+  sheet: 1,
+  pillow: 2
+};
+Object.setPrototypeOf(bed, table);
+
+let pockets = {
+  money: 2000
+};
+Object.setPrototypeOf(pockets, bed);
+
+// Exercise 2
+let hamster = {
+  stomach: [],
+  eat(food) {
+    this.stomach = [food]; // Assign new array to avoid prototype sharing
+  }
+};
+
+let speedy = {};
+Object.setPrototypeOf(speedy, hamster);
+
+let lazy = {};
+Object.setPrototypeOf(lazy, hamster);
